09c1a88591e285349baeebeea3bc25828c7f4bb3
pedroproni-dev <pedroproni-dev@users.noreply.github.com>
2026-05-11 08:18:02 -0400

    Portfolio Implementation

---
+++ api/Controllers/PortfolioController.cs
@@ -0,0 +1,96 @@
+using api.Extensions;
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepo;
+        private readonly IPortfolioRepository _portfolioRepo;
+        private readonly ITwelveDataService _twelveDataService;
+
+        public PortfolioController(
+            UserManager<AppUser> userManager,
+            IStockRepository stockRepo,
+            IPortfolioRepository portfolioRepo,
+            ITwelveDataService twelveDataService)
+        {
+            _userManager = userManager;
+            _stockRepo = stockRepo;
+            _portfolioRepo = portfolioRepo;
+            _twelveDataService = twelveDataService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserPortfolio()
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser!);
+            return Ok(userPortfolio);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddPortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var stock = await _stockRepo.GetBySymbolAsync(symbol);
+
+            if (stock == null)
+            {
+                stock = await _twelveDataService.FindStockBySymbolAsync(symbol);
+                if (stock == null) return BadRequest("Stock does not exist");
+
+                await _stockRepo.CreateAsync(stock);
+            }
+
+            if (stock == null) return BadRequest("Stock not found");
+
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser!);
+
+            if (userPortfolio.Any(e => e.Symbol.ToLower() == symbol.ToLower()))
+                return BadRequest("Cannot add same stock to portfolio");
+
+            var portfolioModel = new Portfolio
+            {
+                StockId = stock.Id,
+                AppUserId = appUser!.Id
+            };
+
+            await _portfolioRepo.CreateAsync(portfolioModel);
+            return Created("", portfolioModel);
+        }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser!);
+            var filteredStock = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower()).ToList();
+
+            if (filteredStock.Count() == 1)
+            {
+                await _portfolioRepo.DeletePortfolioAsync(appUser!, symbol);
+            }
+            else
+            {
+                return BadRequest("Stock not in your portfolio");
+            }
+
+            return Ok();
+        }
+    }
+}
