5236cba63e064cfbb10ff686c7b779796c180d1a
pedroproni-dev <pedroproni-dev@users.noreply.github.com>
2026-05-12 11:06:27 -0400

    Update: Switch database provider from SQLite/SQLServer to PostgreSQL

--- api/Program.cs
+++ api/Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddSwaggerGen(option =>
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
